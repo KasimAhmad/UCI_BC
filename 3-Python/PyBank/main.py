f6ff0813a6f2c373cbffeba7fab4801bccb462e8
Kasim <kasim.ahmad95@yahoo.com>
2020-09-30 00:05:08 -0700

    Adding Python Homework Week 3

--- 3-Python/PyBank/main.py
+++ 3-Python/PyBank/main.py
@@ -1,1 +1,58 @@
+import os
+import csv 
+
+csvpath = os.path.join("Resources", "budget_data.csv")
+# Intialize variable to increment in loop
+num_months = 0
+# Blank list to capture all changes
+net_changes = []
+# Min and Max increase/decrease
+greatest_increase = [0,0]
+greatest_decrease = [0,0]
+total_pl = 0
+prev_pl = 0
+pl = 0
+# CSV Path
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f'CSV Header: {csv_header}')
+# For loop with enumerate to account for total averages changes    
+    for i, row in enumerate(csvreader):
+        print(row)
+        num_months = num_months + 1
+        pl = int(row[1])
+        current_month = row[0]
+        total_pl = total_pl + pl    
+        if i == 0:
+            prev_pl = pl   
+        else:
+            change = pl - prev_pl 
+            prev_pl = pl
+            net_changes.append(change)
+            if change > greatest_increase[1]:
+                greatest_increase[0] = current_month
+                greatest_increase[1] = change
+            if change < greatest_decrease[1]:
+                greatest_decrease[0] = current_month
+                greatest_decrease[1] = change
+    net_changes = sum(net_changes) / len(net_changes)
+    net_changes = float("{:.2f}".format(net_changes))
+# Print statements
+summary = (f"""\nFinancial Analysis\n
+    ----------------------------\n
+    Total Months: {num_months}\n
+    Total: ${total_pl}\n
+    Average  Change: ${net_changes}\n
+    Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n
+    Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n""")
+#Print Summary
+print(summary)
+# Export
+export_name = os.path.join("Analysis", "budget_analysis.txt")
+with open(export_name, "a") as txt:
+    txt.write(summary)
+
+
 
--- 3-Python/PyPoll/main.py
+++ 3-Python/PyPoll/main.py
@@ -1,1 +1,45 @@
+import os
+import csv 
+total_votes = 0
+candidate = {}
+candidate_options = []
+candidate_percentage = []
+winning_vote_count = 0
+winner = 0
 
+csvpath = os.path.join("Resources", "election_data.csv")
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f'CSV Header: {csv_header}')
+    for row in (csvreader):
+        total_votes = total_votes + 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate[candidate_name] = 1
+        else:
+            candidate[candidate_name] += 1
+for unique_voter in candidate_options:
+    candidate_percentage.append(round(candidate[unique_voter] / total_votes * 100, 2))
+    if candidate[unique_voter] > winning_vote_count:
+        winning_vote_count = candidate[unique_voter]
+        winner = unique_voter
+
+summary = (f"""\nVoter Analysis\n
+    ----------------------------\n
+    Total number of votes: {total_votes}\n
+     ----------------------------\n
+    Khan: {candidate_percentage[0]:.3f}% ({candidate["Khan"]})
+    Correy: {candidate_percentage[1]:.3f}% ({candidate["Correy"]})
+    Li: {candidate_percentage[2]:.3f}% ({candidate["Li"]})
+    O'Tooley: {candidate_percentage[1]:.3f}% ({candidate["O'Tooley"]})\n
+    Winner: {winner}""")
+
+print(summary)
+
+# Export
+export_name = os.path.join("Analysis", "election_analysis.txt")
+with open(export_name, "a") as txt:
+    txt.write(summary)
